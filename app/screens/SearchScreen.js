35a362366a3bfde85bf0e519be01c391c6d8d7d8
kishorchouhan <devkishorbwr@gmail.com>
2019-08-11 20:38:31 +0530

    final commit

--- app/screens/SearchScreen.js
+++ app/screens/SearchScreen.js
@@ -17,51 +17,6 @@ export default class SearchScreen extends React.Component {
     this.navigateToCharacterScreen = this.navigateToCharacterScreen.bind(this);
     this.renderItem = this.renderItem.bind(this);
     this.onSearch = this.onSearch.bind(this);
-    // this.data = [
-    //   {
-    //     name: "Iron Cross Army",
-    //     thumbnailUrl:
-    //       "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg",
-    //     description: ""
-    //   },
-    //   {
-    //     name: "Iron Fist (Bei Bang-Wen)",
-    //     thumbnailUrl:
-    //       "http://i.annihil.us/u/prod/marvel/i/mg/9/20/53176ebd40ad7.jpg",
-    //     description: ""
-    //   },
-    //   {
-    //     name: "Iron Fist (Danny Rand)",
-    //     thumbnailUrl:
-    //       "http://i.annihil.us/u/prod/marvel/i/mg/3/f0/52616788ebc63.jpg",
-    //     description: ""
-    //   },
-    //   {
-    //     name: "Iron Fist (Orson Randall)",
-    //     thumbnailUrl:
-    //       "http://i.annihil.us/u/prod/marvel/i/mg/6/e0/53176e979cca2.jpg",
-    //     description: ""
-    //   },
-    //   {
-    //     name: "Iron Fist (Quan Yaozu)",
-    //     thumbnailUrl:
-    //       "http://i.annihil.us/u/prod/marvel/i/mg/4/20/53176e89b563e.jpg",
-    //     description: ""
-    //   },
-    //   {
-    //     name: "Iron Fist (USM)",
-    //     thumbnailUrl:
-    //       "http://i.annihil.us/u/prod/marvel/i/mg/6/20/52321751dffa6.jpg",
-    //     description:
-    //       "Danny is always focused yet relaxed - more Zen-focused than chill surfer dude."
-    //   },
-    //   {
-    //     name: "Iron Fist (Wu Ao-Shi)",
-    //     thumbnailUrl:
-    //       "http://i.annihil.us/u/prod/marvel/i/mg/7/03/53176f05a6851.jpg",
-    //     description: ""
-    //   }
-    // ];
     this.renderItem = this.renderItem.bind(this);
     this.navigateToCharacterScreen = this.navigateToCharacterScreen.bind(this);
   }
